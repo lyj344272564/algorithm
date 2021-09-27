5751e814d488103073563f77afcadf908ed8380c
Richard <344272564@qq.com>
2021-09-27 09:43:43 +0800

    算法基础day04

--- AcWing/src/com/richard/sfjclx/AC2816.java
+++ AcWing/src/com/richard/sfjclx/AC2816.java
@@ -1,5 +1,6 @@
 package com.richard.sfjclx;
 
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -9,28 +10,34 @@ public class AC2816 {
 
     private static final int N = 100010;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Scanner scanner = new Scanner(System.in);
+        String[] val = br.readLine().split(" ");
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        int n = Integer.parseInt(val[0]);
+        int m = Integer.parseInt(val[1]);
+        int x = Integer.parseInt(val[2]);
 
-        int[] a = new int[N];
-        int[] b = new int[N];
+        int[] a = new int[n];
+        int[] b = new int[m];
 
-
+        String[] curA = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            a[i] = scanner.nextInt();
+            a[i] = Integer.parseInt(curA[i]);
         }
 
-        for (int j=0; j<m; j++) {
-            b[j] = scanner.nextInt();
+        String[] curB = br.readLine().split(" ");
+        for (int i=0; i<m; i++) {
+            b[i] = Integer.parseInt(curB[i]);
         }
 
         int i=0;
         for (int j=0; j<m; j++) {
-            if (a[i] == b[j] && i<n) i++;
+            if (a[i] == b[j] && i<n) {
+                i++;
+            }
         }
         if (i==n) {
             System.out.println("Yes");
--- AcWing/src/com/richard/sfjclx/AC800.java
+++ AcWing/src/com/richard/sfjclx/AC800.java
@@ -1,6 +1,7 @@
 package com.richard.sfjclx;
 
 
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -9,36 +10,81 @@ import java.util.Scanner;
 public class AC800 {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int x = sc.nextInt();
+        String[] val = br.readLine().split(" ");
+
+        int n = Integer.parseInt(val[0]);
+        int m = Integer.parseInt(val[1]);
+        int x = Integer.parseInt(val[2]);
 
         int[] a = new int[n];
         int[] b = new int[m];
 
+        String[] curA = br.readLine().split(" ");
         for (int i=0; i<n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(curA[i]);
         }
 
-        for (int j=0; j<m; j++) {
-            b[j] = sc.nextInt();
+        String[] curB = br.readLine().split(" ");
+        for (int i=0; i<m; i++) {
+            b[i] = Integer.parseInt(curB[i]);
         }
 
         for (int i=0,j=m-1; i<n; i++) {
-            // 这里j 不能=0 里面一减就是-1此时就会越界
-           while (j>0 && (x < a[i] + b[j])) {
-               j--;
-           }
-           if (j>=0 && a[i]+b[j] == x) {
-               System.out.println(i+" "+j);
-           }
+            while (j>0 && (x<a[i]+b[j])) {
+                j--;
+            }
+            if (j>=0 && x==a[i]+b[j]) {
+//                System.out.println(i + " " + j);
+                bw.write(i + " " + j);
+               break;
+            }
         }
 
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
+
+
+//
+//    public static void main(String[] args) {
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        int x = sc.nextInt();
+//
+//        int[] a = new int[n];
+//        int[] b = new int[m];
+//
+//        for (int i=0; i<n; i++) {
+//            a[i] = sc.nextInt();
+//        }
+//
+//        for (int j=0; j<m; j++) {
+//            b[j] = sc.nextInt();
+//        }
+//
+//        for (int i=0,j=m-1; i<n; i++) {
+//            // 这里j 不能=0 里面一减就是-1此时就会越界
+//           while (j>0 && (x < a[i] + b[j])) {
+//               j--;
+//           }
+//           if (j>=0 && a[i]+b[j] == x) {
+//               System.out.println(i+" "+j);
+//               return;
+//           }
+//        }
+//
+//
+//    }
 
 }
