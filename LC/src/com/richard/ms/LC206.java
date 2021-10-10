0acac239c171ddfdb48a09fab711494efd01bdbe
Richard <344272564@qq.com>
2021-10-10 12:40:24 +0800

    LCms

---
+++ LC/src/com/richard/ms/LC206.java
@@ -0,0 +1,29 @@
+package com.richard.ms;
+
+import javax.swing.event.MenuKeyListener;
+
+/**
+ * 206. 反转链表
+ */
+public class LC206 {
+
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+
+    }
+
+}
