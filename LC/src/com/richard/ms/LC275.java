4dfc7f234fa490d864bbb5257822fe34072bd6eb
Richard <344272564@qq.com>
2021-10-19 22:24:11 +0800

    ms

--- LC/src/com/richard/ms/LC275.java
+++ LC/src/com/richard/ms/LC275.java
@@ -1,5 +1,7 @@
 package com.richard.ms;
 
+import java.util.Arrays;
+
 /**
  * 275. H 指数 II
  */
@@ -7,9 +9,29 @@ public class LC275 {
 
     public int hIndex(int[] citations) {
 
+        int n = citations.length;
 
+        int l = 0;
+        int r = n - 1;
 
-        return 0;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+
+            if (citations[n-mid] >= mid) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+
+        }
+        return n-l;
+//        int n = citations.length;
+//        for (int i=n; i>0; i--) {
+//            if (citations[n-i] >= i) {
+//                return i;
+//            }
+//        }
+//        return 0;
     }
 
 }
